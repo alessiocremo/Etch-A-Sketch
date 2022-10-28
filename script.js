e35a344ee5798f4a707d0b79118ed9a9a91bd691
alessiocremo <alessiocremo@DESKTOP-L55JODL.localdomain>
2022-10-28 19:58:24 +0300

    still need to fix random, clear board

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
+let squaresColor="black"
+let size=16
+
 document.addEventListener("DOMContentLoaded", function(){
-    createBoard(16);
-    console.log("hi")
+    squaresColor="black"
+    createBoard(size);
 })
 
-function createBoard(size){
-    let board = document.querySelector(".board");
 
-    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+setSizeButton = document.getElementById("change-size")
+setSizeButton.addEventListener("click", setNewSize())
 
-    let numDivs = size*size;
 
-    for (let i=0; i<numDivs; i++){
-        let div = document.createElement("div");
-        div.style.backgroundColor="black"
-        board.insertAdjacentElement("beforeend",div)
-    }
-
+function setNewSize(){
+    createBoard(document.getElementById("set-size").value)
 }
 
 
+
+function createBoard(boardSize){
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${boardSize}, 1fr)`
+    board.style.gridTemplateRows = `repeat(${boardSize}, 1fr)`
+
+    let numDivs = boardSize*boardSize
+
+    for (let i=0; i<numDivs; i++){
+        let div = document.createElement("div");
+        div.addEventListener("mouseover",function(){
+            div.style.backgroundColor=squaresColor
+        })
+        // div.style.backgroundColor="black"
+        board.insertAdjacentElement("beforeend",div)
+    }
+}
+
+function setColor(newColor){
+    squaresColor=newColor
+}
